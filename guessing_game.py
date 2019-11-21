1c3bd582b82ead6a5381dd4fa62f8bbb9222dcf9
Stefan Gherman <scgheman@gmail.com>
2019-11-21 21:45:52 +0200

    Remove duplicates input if guess was high or low.

--- guessing_game.py
+++ guessing_game.py
@@ -8,18 +8,17 @@ numbers_up_to_99 = []
 for number in range(NUMBERS_IN_LIST):
     numbers_up_to_99.append(random.randint(1, 99))
 
-
+print(numbers_up_to_99)
 for index in range(10):
     guess = int(input("Enter an integer from 1 to 99: "))
     while numbers_up_to_99[index] != guess:
         if guess < numbers_up_to_99[index]:
             print("guess is low")
-            guess = int(input("Enter an integer from 1 to 99: "))
         elif guess > numbers_up_to_99[index]:
             print("guess is high")
-            guess = int(input("Enter an integer from 1 to 99: "))
         else:
             break
+        guess = int(input("Enter an integer from 1 to 99: "))
     print("you guessed it!")
 
 numbers_up_to_49 = []
@@ -31,10 +30,9 @@ for index in range(10):
     while numbers_up_to_49[index] != guess:
         if guess < numbers_up_to_49[index]:
             print("guess is low")
-            guess = int(input("Enter an integer from 1 to 49: "))
         elif guess > numbers_up_to_49[index]:
             print("guess is high")
-            guess = int(input("Enter an integer from 1 to 49: "))
         else:
             break
+        guess = int(input("Enter an integer from 1 to 49: "))
     print("you guessed it!")
