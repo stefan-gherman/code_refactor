acf9d37c8b1003a562f06d9656e16903e0c45f2e
Stefan Gherman <scgheman@gmail.com>
2019-11-21 21:54:15 +0200

    Remove magic numbers from the for loop of the main guessing logic.
    Instead of iterating until an arbitrary hardcoded number is reached, the loop will execute until all elements from the list have been checked.
    
    This eliminates the possibility to have the loop go out of the indexed bounds of the list.

--- guessing_game.py
+++ guessing_game.py
@@ -8,8 +8,7 @@ numbers_up_to_99 = []
 for number in range(NUMBERS_IN_LIST):
     numbers_up_to_99.append(random.randint(1, 99))
 
-print(numbers_up_to_99)
-for index in range(10):
+for index in range(len(numbers_up_to_99)):
     guess = int(input("Enter an integer from 1 to 99: "))
     while numbers_up_to_99[index] != guess:
         if guess < numbers_up_to_99[index]:
@@ -25,7 +24,7 @@ numbers_up_to_49 = []
 for number in range(NUMBERS_IN_LIST):
     numbers_up_to_49.append(random.randint(1, 49))
 
-for index in range(10):
+for index in range(len(numbers_up_to_49)):
     guess = int(input("Enter an integer from 1 to 49: "))
     while numbers_up_to_49[index] != guess:
         if guess < numbers_up_to_49[index]:
